b8372c85df79be4ff5a4b7135a1fed050fe06084
sunsg <sunsgjob@163.com>
2026-03-12 14:28:38 +0800

    部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://easysunsg.github.io',
-    //base: '/easysunsg.github.io',
+    base: '/',
     integrations: [tailwind(), mdx()]
 });
